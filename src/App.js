7b172556e26e8840577561e337fdcc3cbfbd7e93
opeyemibioku <opeyemibioku@gmail.com>
2023-06-26 13:37:07 +0100

    worked on the document embed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home";
+import LegalRenumerationOrder from "./pages/document";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route
+          path="Legal-Practitioners-Renumeration-Order-2023"
+          element={<LegalRenumerationOrder />}
+        />
       </Routes>
     </BrowserRouter>
   );
