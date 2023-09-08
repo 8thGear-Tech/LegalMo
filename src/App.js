d5fb6b708616c1db58ea52386d2e3822884f1d41
opeyemibioku <opeyemibioku@gmail.com>
2023-09-08 14:10:18 +0100

    updated the sass file import

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import Landing from "./pages/Website/Landing";
 import LegalRenumerationOrder from "./pages/document";
 import About from "./pages/Website/About";
 import Products from "./pages/Website/Products";
-import {CompanySignUp, NextCompanySignUp} from "./pages/Company/CompanySignUp";
-import {LawyerSignUp, NextLawyerSignUp} from "./pages/Lawyer/LawyerSignUp";
+import {
+  CompanySignUp,
+  NextCompanySignUp,
+} from "./pages/Company/CompanySignUp";
+import { LawyerSignUp, NextLawyerSignUp } from "./pages/Lawyer/LawyerSignUp";
 import AdminSignUp from "./pages/Admin/AdminSignUp";
 import Login from "./pages/Website/Login";
 import Dashboard from "./pages/Company/Dashboard";
@@ -13,16 +16,15 @@ import PasswordReset from "./pages/Website/PasswordReset";
 import NewPassword from "./pages/Website/NewPassword";
 import OTP from "./pages/Website/OTP";
 
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/landing" element={<Landing />} />
-        <Route path="/products" element={<Products />} />
+        {/* <Route path="/products" element={<Products />} /> */}
         <Route path="/about-us" element={<About />} />
-        <Route path="/login" element={<Login/>} />
+        {/* <Route path="/login" element={<Login/>} />
         <Route path="/lawyer-signup" element={<LawyerSignUp/>} />
         <Route path="/next-lawyer-signup" element={<NextLawyerSignUp/>} />
         <Route path="/company-signup" element={<CompanySignUp/>} />
@@ -31,7 +33,7 @@ function App() {
         <Route path="/password-reset" element={<PasswordReset/>} />
         <Route path="/otp" element={<OTP/>} />
         <Route path="/new-password" element={<NewPassword/>} />
-        <Route path="/products" element={<Dashboard/>} />
+        <Route path="/products" element={<Dashboard/>} /> */}
         <Route
           path="Legal-Practitioners-Renumeration-Order-2023"
           element={<LegalRenumerationOrder />}
