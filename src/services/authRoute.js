1d24f7319d2801dee843f9136a0e73afb0bc4225
opeyemibioku <opeyemibioku@gmail.com>
2024-02-06 11:23:21 +0100

    updated the cart component

--- src/services/authRoute.js
+++ src/services/authRoute.js
@@ -692,7 +692,7 @@ export default () => {
       axios
         .post("/api/logout")
         .then(async (response) => {
-          navigate("/home");
+          navigate("/");
         })
 
         .catch((e) => {
