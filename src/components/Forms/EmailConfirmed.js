91d8087ba30899dd2487a8f0f38b35b74b8a7fc4
opeyemibioku <opeyemibioku@gmail.com>
2024-02-07 11:43:05 +0100

    updated the email confirmation modal

--- src/components/Forms/EmailConfirmed.js
+++ src/components/Forms/EmailConfirmed.js
@@ -1,21 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import check from '../../assets/images/check.svg'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import check from "../../assets/images/check.svg";
 
 const EmailConfirmedModal = () => {
   const navigate = useNavigate();
   const [showModal, setShowModal] = useState(true);
 
- 
   const closeModal = () => {
     setShowModal(false);
-    navigate('/login'); 
+    navigate("/login");
   };
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      navigate('/login');
-    }, 3000);
+      navigate("/login");
+    }, 30000);
 
     return () => clearTimeout(timer);
   }, [navigate]);
@@ -23,21 +22,30 @@ const EmailConfirmedModal = () => {
   return (
     <div>
       <div
-        className={`modal fade px-3 ${showModal ? 'show' : ''}`}
-        style={{ display: showModal ? 'block' : 'none'}}
-        tabIndex='-1'
-        role='dialog'
-        onClick={closeModal} 
+        className={`modal fade px-3 ${showModal ? "show" : ""}`}
+        style={{ display: showModal ? "block" : "none" }}
+        tabIndex="-1"
+        role="dialog"
+        onClick={closeModal}
       >
-        <div className='modal-dialog modal-dialog-centered' role='document'>
-          <div className='modal-content p-sm-5 p-3' style={{backgroundColor:'#D9DAF9'}}>
-            <div className='d-flex justify-content-end'>
-              <button type='button' className='btn-close' onClick={closeModal}></button>
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div
+            className="modal-content p-sm-5 p-3"
+            style={{ backgroundColor: "#D9DAF9" }}
+          >
+            <div className="d-flex justify-content-end">
+              <button
+                type="button"
+                className="btn-close"
+                onClick={closeModal}
+              ></button>
             </div>
-            <div className='modal-body text-center'>
-              <div className='' role='alert'>
-                <img src={check} alt='check' style={{ width: '130px' }} />
-                <h4 className='mt-5' style={{ fontWeight: '700' }}>Email confirmed successfully</h4>
+            <div className="modal-body text-center">
+              <div className="" role="alert">
+                <img src={check} alt="check" style={{ width: "130px" }} />
+                <h4 className="mt-5" style={{ fontWeight: "700" }}>
+                  Email confirmed successfully
+                </h4>
                 <p>You can now log in</p>
               </div>
             </div>
@@ -45,12 +53,11 @@ const EmailConfirmedModal = () => {
         </div>
       </div>
       <div
-        className={`modal-backdrop fade ${showModal ? 'show' : ''}`}
-        style={{ display: showModal ? 'block' : 'none' }}
+        className={`modal-backdrop fade ${showModal ? "show" : ""}`}
+        style={{ display: showModal ? "block" : "none" }}
       ></div>
     </div>
   );
 };
 
 export default EmailConfirmedModal;
-
